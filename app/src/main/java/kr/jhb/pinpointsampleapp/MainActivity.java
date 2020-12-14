de8957b238f4ff39c96a7b40ee4ad4d594b02b02
jakemraz <jakemraz100@gmail.com>
2020-12-15 01:36:27 +0900

    make be able to get push notification on foreground

--- app/src/main/java/kr/jhb/pinpointsampleapp/MainActivity.java
+++ app/src/main/java/kr/jhb/pinpointsampleapp/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                     applicationContext,
                     AWSMobileClient.getInstance(),
                     awsConfig);
-
+            pinpointConfig.withPostNotificationsInForeground(true);
             pinpointManager = new PinpointManager(pinpointConfig);
 
             FirebaseInstanceId.getInstance().getInstanceId()
